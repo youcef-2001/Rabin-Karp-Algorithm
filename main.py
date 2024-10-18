c7bf1be94d35ba8a9208c0a391b1b0b691844e50
baleh <youcef.baleh@etu.u-paris.fr>
2024-10-18 15:48:20 +0200

    petite commande pour le main

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import time
 from implementations.implem1 import search
 from implementations.implem import Searcher , Hasher
-from ressources.chaines import texte1 , texte2 , texte3
+
+
 
 if __name__ == '__main__':
-	pat = "hey"		
-	# les args 
-	# A prime number
+	pat = input("quelle patterne rechercher? ")	
+	texte = input("Le texte dans lequel recherchez")
+	# nombre premier
 	q = 101
 
-		# Function Call
+	# Function Call
 	print("Resultat avec l'algo fonctionnel :")
 	start = time.time()
-	print (search(pat, texte2, q))
+	print (search(pat, texte, q))
 	end = time.time()
 	timeT = end- start
-	print("time" , timeT)
+	print("time" , timeT,"s")
+
 
 
 	print("Resultat avec l'algo POO :")
 	#Function Call with POO 
 	start = time.time()
-	hasher = Hasher(texte2, q, len(pat))
+	hasher = Hasher(texte, q, len(pat))
 	hasher.calculHashes()# rempli le dictionnaire des hash 
 	searcher = Searcher(hasher , pat)
 	print(searcher.searchPattern())
 	end = time.time()
 	timeT= end-start
-	print("time" ,timeT )
+	print("time" ,timeT , "s")
 	
 
 
